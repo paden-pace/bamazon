987bcc43a64b1192531067ba14288dd2ce8fe24c
Paden Pace <pjpace27@yahoo.com>
2017-04-09 14:30:59 -0500

    Functionality to Customer Option and Location for Admin Option

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 
 var inquirer = require("fs");
-var inquirer = require("file-share");
+//var inquirer = require("file-share");
 var inquirer = require("inquirer");
 var mysql = require('mysql');
 
@@ -12,25 +12,27 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function (err) {
-    if (err) throw (error);
-    console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function (err) {
+//     if (err) throw (error);
+//     console.log("connected as id " + connection.threadId);
+// });
 
 var options = function(){
   inquirer.prompt([
         {
             type: "list",
-            message: "What would you like to do?",
-            choices: ["Let's Go Shopping", "exit"],
+            message: "Who are you?",
+            choices: ["Customer", "Administrator", "exit"],
             name: "selection"
         }
     ]).then(function(user) {
         switch (user.selection) {
-            case "Let's Go Shopping":
-                shopping();
+            case "Customer":
+                cust();
                 //options();
                 break;
+            case "Administrator":
+                admin();
                 
             case "exit":
                 break;
@@ -38,10 +40,13 @@ var options = function(){
     });
 };
 
+var admin = function() {
+    console.log("Hello Admin!");
+};
 
-function shopping() {
-    console.log("artist data");
-          inquirer.prompt([
+var cust = function() {
+    console.log("Welcome to this super trendy store!");
+    inquirer.prompt([
         {
             type: "list",
             message: "What would you like to do?",
@@ -81,9 +86,80 @@ var seeItems = function() {
     });
 };
 
+var purchaseReq = function (deptListItem, deptQuant){
+    connection.query('Select * FROM products WHERE product_name = ?', [deptListItem], function (err,results){
+        var trueQuant = results[0].stock_quantity;
+        // console.log("Request: " + dept.quant);
+        // console.log("Amount in Stock: " + trueQuant);
+        if (deptQuant > trueQuant){
+            console.log("Looks like we don't have enough in stock.");
+            console.log("We only have: " + trueQuant);
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "How many would you like to order?",
+                    name: "quant"
+                }
+            ]).then(function(update) {
+                purchaseReq(deptListItem, update.quant);
+            });
+            
+        } else if (deptQuant <= trueQuant){
+            console.log("Sure thing buddy!");
+            var newQuant = (trueQuant - deptQuant);
+            connection.query('UPDATE products SET ? WHERE ?', [
+                {stock_quantity: newQuant},
+                {product_name: deptListItem}
+                ], function(err, results){
+                    if(err) throw err;
+                });
+            console.log("New Quantity in Stock is: " + newQuant);
+        };
+    });
+};
+
 var makeSelect = function() {
-    console.log("Pick an item");
+    // connection.query('Select * FROM products', function (err,results){
+    //     var newAll = [];
+    //     for (i=0; i<results.length; i++){
+    //         newAll.push(results[i].department_name);
+    //     };
+    // });
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What department do you want to look into?",
+            choices: ["Clothing", "Electronics", "Furniture"],
+            name: "dept"
+        }
+    ]).then(function(select) {
+        //console.log ("Item: "+ select.item);
+        //console.log ("Department: " + select.dept);
+        //console.log ("Quantity: " + select.quant);
+        connection.query('Select * FROM products WHERE department_name = ?', [select.dept], function (err,results){
+            var newDept = [];
+            for (i=0; i<results.length; i++){
+                newDept.push(results[i].product_name);
+            };
+            inquirer.prompt([
+                {
+                    type: "list",
+                    message: "What item would you like to buy?",
+                    choices: newDept,
+                    name: "listItem"
+                },
+                {
+                    type: "input",
+                    message: "How many of those would you like?",
+                    name: "quant"
+                }
+            ]).then(function(dept) {
+                console.log(dept.listItem);
+                purchaseReq(dept.listItem, dept.quant);
+            });
+        });
+    });
 };
 
 
- options();
+options();
